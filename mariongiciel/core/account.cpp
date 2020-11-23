840d12ce7074ea4d9033b158112fb2b95687ad61
auzou <senecal.c.pro@hotmail.fr>
2020-11-23 16:00:11 +0100

    Update account.cpp

--- mariongiciel/core/account.cpp
+++ mariongiciel/core/account.cpp
@@ -3,93 +3,34 @@
 mariongiciel::core::Account::Account(QObject *parent)
     : QObject(parent)
 {
-    using namespace mariongiciel::global::rcs;
 
-    if(!this->fileExist() && !this->fileIsValid())
-    {
-
-    }
 }
 
-bool mariongiciel::core::Account::fileExist()
+void mariongiciel::core::Account::createAccount(AccountData accountData)
 {
-    using namespace mariongiciel::global::rcs;
+    QJsonDocument document;
+    QJsonObject mainObject {
+        {"key", accountData.key},
+        {"id", accountData.id},
+    };
 
-    QFile file;
-    if(file.exists(account::_ACCOUNT_+account::_DEFAULT_FORMAT_))
-    {
-        this->format = account::_DEFAULT_FORMAT_;
-    } else if(file.exists(account::_ACCOUNT_+account::_COMPRESS_FORMAT_)) {
-        this->format = account::_COMPRESS_FORMAT_;
-    } else {
-        file.close();
-        return false;
-    }
-
-    this->location = account::_ACCOUNT_+this->format;
-    file.close();
-    return true;
-}
-
-bool mariongiciel::core::Account::fileIsValid()
-{
-    QString data = this->getFileData();
-    if(data.isEmpty())
-    {
-        return false;
-    }
-
-    QJsonDocument document = QJsonDocument::fromJson(data.toUtf8());
-    QJsonObject mainObject = document.object();
-    if(!mainObject["key"].isString() || !mainObject["id"].isString())
-    {
-        // create account
-        return false;
-    }
-
-    return true;
-}
-
-QString mariongiciel::core::Account::getFileData()
-{
-    using namespace mariongiciel::global::rcs;
-
-    QFile file(this->location);
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-        file.close();
-        return "";
-    }
-    QByteArray data = file.readAll();
-
-    if("."+QFileInfo(file).completeSuffix() == account::_COMPRESS_FORMAT_)
-    {
-        //add compress format
-        //add AES256
-        //add 7zip lib
-    }
-    file.close();
-    return data;
-}
-
-void mariongiciel::core::Account::createAccount()
-{
-    //empty section
+    document.setObject(mainObject);
+    FileManagement::write(document.toJson(), global::rcs::account::_ACCOUNT_);
 }
 
 mariongiciel::core::AccountData mariongiciel::core::Account::getAccountData()
 {
-    QString data = this->getFileData();
-
     AccountData accountData;
-
-    QJsonDocument document = QJsonDocument::fromJson(data.toUtf8());
-    QJsonObject mainObject = document.object();
-    if(this->fileIsValid())
+    if(!FileManagement::isExist(global::rcs::account::_ACCOUNT_))
     {
-        accountData.id = mainObject["id"].toString();
-        accountData.key = mainObject["key"].toString();
+        return accountData;
     }
+
+    QJsonDocument document = QJsonDocument::fromJson(FileManagement::readFile(global::rcs::account::_ACCOUNT_).toUtf8());
+    QJsonObject mainObject = document.object();
+    accountData.id = mainObject["id"].toString();
+    accountData.key = mainObject["key"].toString();
+
     return accountData;
 }
 
