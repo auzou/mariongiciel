93c18daaee2c06e8f797fea82aa4b996c7242491
auzou <senecal.c.pro@hotmail.fr>
2020-11-23 16:00:48 +0100

    Update request.cpp

--- mariongiciel/core/network/request.cpp
+++ mariongiciel/core/network/request.cpp
@@ -1,5 +1,6 @@
 #include "request.h"
 
+/* ********* REQUEST ********* */
 
 mariongiciel::core::network::Request::Request(QObject *parent)
     :QObject(parent)
@@ -39,7 +40,7 @@ void mariongiciel::core::network::Request::sendRequest(
 #endif
             emit requestError(reply->errorString());
         } else{
-            qDebug()<< QString(reply->rawHeader("Content-Range"));
+            qDebug()<<reply->rawHeader("Content-Range");
             emit requestFinished(QString(reply->rawHeader("Content-Range")), reply->readAll());
         }
     });
@@ -78,7 +79,110 @@ void mariongiciel::core::network::Request::sendRequest(
 #endif
 
 }
+
 mariongiciel::core::network::Request::~Request() noexcept
 {
 
+}
+
+/* ********* REQUESTBASE ********* */
+
+/* ********* CONSTRUCT/DESTRUCT ********* */
+mariongiciel::core::network::RequestBase::RequestBase(QUrl url, QObject  *parent)
+    : QObject(parent), url(url)
+{
+    this->requestManager = new QNetworkAccessManager(this);
+    this->sslConfiguration.setProtocol(QSsl::TlsV1_2); // default
+}
+
+mariongiciel::core::network::RequestBase::RequestBase(QString url, QObject  *parent)
+    : QObject(parent), url(url)
+{
+    this->requestManager = new QNetworkAccessManager(this);
+    this->sslConfiguration.setProtocol(QSsl::TlsV1_2); // default
+}
+
+mariongiciel::core::network::RequestBase::~RequestBase() noexcept
+{
+
+}
+
+/* ********* ACCESSOR ********* */
+
+void mariongiciel::core::network::RequestBase::setHeaders(QMap<QString, QString> headers)
+{
+    this->headers = headers;
+}
+
+const QMap<QString, QString> mariongiciel::core::network::RequestBase::setHeaders() const
+{
+    return this->headers;
+}
+
+void mariongiciel::core::network::RequestBase::setQueryParam(QUrlQuery queryParam)
+{
+    this->queryParam = queryParam;
+}
+
+void mariongiciel::core::network::RequestBase::setQueryParam(const QMap<QString, QString> &body)
+{
+    QUrlQuery queryParam;
+    for(auto &i : body)
+    {
+        queryParam.addQueryItem(i, body.value(i));
+    }
+}
+
+
+const QUrlQuery mariongiciel::core::network::RequestBase::getQueryParam() const
+{
+    return this->queryParam;
+}
+
+void mariongiciel::core::network::RequestBase::setSslConfiguration(QSslConfiguration sslConfiguration)
+{
+    this->sslConfiguration = sslConfiguration;
+}
+
+QSslConfiguration mariongiciel::core::network::RequestBase::getSslConfiguration() const
+{
+    return this->sslConfiguration;
+}
+
+/* ********* METHOD ********* */
+
+void mariongiciel::core::network::RequestBase::sendRequest(QNetworkAccessManager::Operation operation)
+{
+    QNetworkRequest request(QUrl(this->url));
+
+    for(auto &i : this->headers.keys())
+    {
+        request.setRawHeader(i.toUtf8(), this->headers.value(i).toUtf8());
+    }
+
+    request.setSslConfiguration(this->sslConfiguration);
+
+    if(QNetworkAccessManager::Operation::PostOperation == operation)
+    {
+        this->requestManager->post(request, queryParam.query().toUtf8());
+    }
+
+    if(QNetworkAccessManager::Operation::GetOperation == operation)
+    {
+        request.setUrl(QUrl(request.url().toString()+queryParam.query().toUtf8()));
+        this->requestManager->get(request);
+    }
+
+    QObject::connect(this->requestManager, &QNetworkAccessManager::sslErrors, [this](QNetworkReply *reply, const QList<QSslError> &errors)->void {
+        emit requestBaseError(reply, errors);
+    });
+
+    QObject::connect(this->requestManager, &QNetworkAccessManager::finished, [this](QNetworkReply *reply)->void {
+        if (reply->error())
+        {
+            emit requestBaseError(reply);
+        } else {
+            emit requestBaseFinished(reply);
+        }
+    });
 }
