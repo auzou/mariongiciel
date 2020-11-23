f15dffad8fba833bc686195dd17778c845ccae07
auzou <senecal.c.pro@hotmail.fr>
2020-11-23 16:01:02 +0100

    Update authentication.cpp

--- mariongiciel/core/network/authentication.cpp
+++ mariongiciel/core/network/authentication.cpp
@@ -1,15 +1,11 @@
 #include "authentication.h"
 
-mariongiciel::core::network::Authentication::Authentication(QObject *parent)
-    : QObject(parent), workInProgress(false)
+mariongiciel::core::network::Authentication::Authentication(RequestBase *parent)
+    : RequestBase("https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=/partenaire", parent),
+      workInProgress(false)
 {
     this->sessionValid = false;
-
-    this->url = "https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=/partenaire";
-
-    this->header = {
-        {"Content-Type", "application/x-www-form-urlencoded"},
-    };
+    //this->setHeaders(QMap<QString, QString>({"Content-Type", "application/x-www-form-urlencoded"}));
 
     this->refreshAccount();
 
@@ -28,13 +24,16 @@ void mariongiciel::core::network::Authentication::refreshAccount()
 {
     mariongiciel::core::Account account;
     mariongiciel::core::AccountData accountData = account.getAccountData();
-    this->body = {
-        //{"realm", "/partenaire"},
+    /*
+    QMap<QString, QString> body {
         {"grant_type", "client_credentials"},
         {"client_id", accountData.id},
         {"client_secret", accountData.key},
         {"scope", "api_offresdemploiv2%20application_"+accountData.id+"%20o2dsoffre"},
     };
+    */
+
+    //this->setQueryParam(body);
 }
 
 bool mariongiciel::core::network::Authentication::getSessionValid() const
@@ -57,7 +56,7 @@ void mariongiciel::core::network::Authentication::runAuth()
     workInProgress = true;
 
     Request *request = new Request(this);
-    request->sendRequest(QNetworkAccessManager::Operation::PostOperation, this->url, this->header, this->body);
+    this->sendRequest(QNetworkAccessManager::Operation::PostOperation);
 
     QObject::connect(request, &Request::requestFinished, [this](QString content, QString data)->void {
         Q_UNUSED(content)
@@ -96,19 +95,3 @@ mariongiciel::core::network::Authentication::~Authentication() noexcept
 }
 
 
-
-mariongiciel::core::network::Authentication* mariongiciel::core::network::AuthSingleton::instance = nullptr;
-
-
-void  mariongiciel::core::network::AuthSingleton::initInstance(QObject *parent)
-{
-    if(AuthSingleton::instance == nullptr)
-    {
-        AuthSingleton::instance = new Authentication(parent);
-    }
-}
-
-mariongiciel::core::network::Authentication*  mariongiciel::core::network::AuthSingleton::GetInstance()
-{
-    return AuthSingleton::instance;
-}
