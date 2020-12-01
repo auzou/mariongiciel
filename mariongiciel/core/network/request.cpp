980b7816b33d708b49ff8fa2b2919815b2ff2dd3
auzou <senecal.c.pro@hotmail.fr>
2020-12-01 18:34:02 +0100

    Update request.cpp

--- mariongiciel/core/network/request.cpp
+++ mariongiciel/core/network/request.cpp
@@ -1,90 +1,5 @@
 #include "request.h"
 
-/* ********* REQUEST ********* */
-
-mariongiciel::core::network::Request::Request(QObject *parent)
-    :QObject(parent)
-{
-    this->requestManager = new QNetworkAccessManager(this);
-}
-
-void mariongiciel::core::network::Request::sendRequest(
-                                                       QNetworkAccessManager::Operation operation,
-                                                       QString url,
-                                                       QVector<QPair<QString,QString>> header,
-                                                       QVector<QPair<QString, QString>> param
-                                                       )
-{
-    QNetworkRequest request(QUrl(url.toUtf8()));
-
-    for(auto &i : header)
-    {
-        request.setRawHeader(i.first.toUtf8(), i.second.toUtf8());
-    }
-
-    QUrlQuery queryParam;
-    for(auto &i : param)
-    {
-        if(!i.second.isEmpty())
-        {
-            queryParam.addQueryItem(i.first, i.second);
-        }
-    }
-
-
-    QObject::connect(this->requestManager, &QNetworkAccessManager::finished, [this](QNetworkReply *reply)->void {
-        if (reply->error())
-        {
-#ifdef QT_DEBUG
-            qDebug()<<"error";
-#endif
-            emit requestError(reply->errorString());
-        } else{
-            qDebug()<<reply->rawHeader("Content-Range");
-            emit requestFinished(QString(reply->rawHeader("Content-Range")), reply->readAll());
-        }
-    });
-
-    QObject::connect(this->requestManager, &QNetworkAccessManager::sslErrors, []()->void {
-#ifdef QT_DEBUG
-        qDebug()<<"ssl error";
-#endif
-    });
-
-#ifdef QT_DEBUG
-    qDebug()<< url <<queryParam.query().toUtf8();
-#endif
-
-    QSslConfiguration sslConfiguration(QSslConfiguration::defaultConfiguration());
-    // ne gere pas v1_3
-    sslConfiguration.setProtocol(QSsl::TlsV1_2);
-    request.setSslConfiguration(sslConfiguration);
-    // post
-    if(operation == QNetworkAccessManager::Operation::PostOperation)
-    {
-        this->requestManager->post(request, queryParam.query().toUtf8());
-    }
-    // get
-    if(operation == QNetworkAccessManager::Operation::GetOperation)
-    {
-        request.setUrl(QUrl(request.url().toString()+queryParam.query().toUtf8()));
-        this->requestManager->get(request);
-    }
-
-#ifdef QT_DEBUG
-    qDebug()
-        << QSslSocket::supportsSsl()
-        << QSslSocket::sslLibraryBuildVersionString()
-        << QSslSocket::sslLibraryVersionString();
-#endif
-
-}
-
-mariongiciel::core::network::Request::~Request() noexcept
-{
-
-}
-
 /* ********* REQUESTBASE ********* */
 
 /* ********* CONSTRUCT/DESTRUCT ********* */
@@ -109,17 +24,17 @@ mariongiciel::core::network::RequestBase::~RequestBase() noexcept
 
 /* ********* ACCESSOR ********* */
 
-void mariongiciel::core::network::RequestBase::setHeaders(QMap<QString, QString> headers)
+void mariongiciel::core::network::RequestBase::setHeader(const QMap<QString, QString> &header)
 {
-    this->headers = headers;
+    this->header = header;
 }
 
-const QMap<QString, QString> mariongiciel::core::network::RequestBase::setHeaders() const
+const QMap<QString, QString> &mariongiciel::core::network::RequestBase::getHeader() const
 {
-    return this->headers;
+    return this->header;
 }
 
-void mariongiciel::core::network::RequestBase::setQueryParam(QUrlQuery queryParam)
+void mariongiciel::core::network::RequestBase::setQueryParam(const QUrlQuery &queryParam)
 {
     this->queryParam = queryParam;
 }
@@ -134,17 +49,17 @@ void mariongiciel::core::network::RequestBase::setQueryParam(const QMap<QString,
 }
 
 
-const QUrlQuery mariongiciel::core::network::RequestBase::getQueryParam() const
+const QUrlQuery &mariongiciel::core::network::RequestBase::getQueryParam() const
 {
     return this->queryParam;
 }
 
-void mariongiciel::core::network::RequestBase::setSslConfiguration(QSslConfiguration sslConfiguration)
+void mariongiciel::core::network::RequestBase::setSslConfiguration(const QSslConfiguration &sslConfiguration)
 {
     this->sslConfiguration = sslConfiguration;
 }
 
-QSslConfiguration mariongiciel::core::network::RequestBase::getSslConfiguration() const
+const QSslConfiguration &mariongiciel::core::network::RequestBase::getSslConfiguration() const
 {
     return this->sslConfiguration;
 }
@@ -155,9 +70,9 @@ void mariongiciel::core::network::RequestBase::sendRequest(QNetworkAccessManager
 {
     QNetworkRequest request(QUrl(this->url));
 
-    for(auto &i : this->headers.keys())
+    for(auto &i : this->header.keys())
     {
-        request.setRawHeader(i.toUtf8(), this->headers.value(i).toUtf8());
+        request.setRawHeader(i.toUtf8(), this->header.value(i).toUtf8());
     }
 
     request.setSslConfiguration(this->sslConfiguration);
