c86d1a206db8ded5cde582a8da14b9327a1a7702
auzou <senecal.c.pro@hotmail.fr>
2020-12-01 18:33:54 +0100

    Update authentication.h

--- mariongiciel/core/network/authentication.h
+++ mariongiciel/core/network/authentication.h
@@ -1,5 +1,16 @@
 #ifndef AUTHENTICATION_NETWORK_H
 #define AUTHENTICATION_NETWORK_H
+
+/**
+ * \file request.h
+ * \brief ...
+ * \author Auzou
+ * \version ..
+ *
+ * ...
+ *
+ */
+
 #include <QObject>
 #include <QVector>
 #include <QPair>
@@ -12,29 +23,46 @@
 #include <QDebug>
 #endif
 
+/**
+ * \namespace mariongiciel::core::network
+ * \brief Network namespace
+ *
+ * Namespace that contains network functionality
+ *
+ */
 namespace mariongiciel::core::network {
 
+/**
+ * \struct Session
+ * \brief Represents a session
+ *
+ */
 struct Session final
 {
-    QString scope;
     int expire;
+    bool valid;
+    QString scope;
     QString tokenType;
     QString token;
 };
 
-
+/**
+ * \class Authentication
+ * \brief Represents an authentication to the API
+ * This class cannot be instantiated
+ * It is accessible from the network/global.h as a singleton
+ */
 class Authentication : public RequestBase
 {
     Q_OBJECT
 
     private :
+         /* my friend and in network/global.h and it's a template */
          template<typename T> friend struct Singleton;
 
     private :
         QTimer *refreshTimer;
         QVector<QPair<QString, QString>> body;
-        QString token;
-        bool sessionValid;
         Session session;
         bool workInProgress;
         explicit Authentication(RequestBase *parent = nullptr);
